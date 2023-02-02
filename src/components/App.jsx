da5fa998e5c8b055c5f95039ac647557877ed6e8
Alekhandro Lago Navarro <lagoalekhandro@gmail.com>
2023-02-02 15:19:16 +0200

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,64 @@
+import { useState } from 'react';
+import {
+  useGetGoodsQuery,
+  useAddProductMutation,
+  useDeleteProductMutation,
+} from 'redux/goodsApi';
+
 export const App = () => {
+  const [count, setCount] = useState('');
+  const [newProduct, setNewProduct] = useState('');
+  const { data = [], isLoading } = useGetGoodsQuery(count);
+  const [addProduct, { isError }] = useAddProductMutation();
+  const [deleteProduct] = useDeleteProductMutation();
+
+  const handleAddProduct = async () => {
+    if (newProduct) {
+      // unwrap => коректна робота всіх пропів({ isError })
+      await addProduct({ name: newProduct }).unwrap();
+      setNewProduct('');
+    }
+  };
+
+  const handlrDeleteProduct = async id => {
+    await deleteProduct(id).unwrap();
+  };
+
+  if (isLoading) return <h1>Loading...</h1>;
+
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
+    <>
+      <div>
+        <input
+          type="text"
+          value={newProduct}
+          onChange={e => setNewProduct(e.target.value)}
+        />
+        <button type="button" onClick={handleAddProduct}>
+          Add product
+        </button>
+      </div>
+
+      <div>
+        <select value={count} onChange={e => setCount(e.target.value)}>
+          <option value="">all</option>
+          <option value="1">1</option>
+          <option value="2">2</option>
+          <option value="3">3</option>
+        </select>
+      </div>
+
+      <ul>
+        {data.map(item => (
+          <li
+            key={item.id}
+            onClick={() => handlrDeleteProduct(item.id)}
+            style={{ cursor: 'pointer' }}
+          >
+            {item.name}
+          </li>
+        ))}
+      </ul>
+    </>
   );
 };
